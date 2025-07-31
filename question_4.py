56b14252273909e8103a920ea8f3ea94cea7e176
Allen Shamrock <allenshamrock37@gmail.com>
2025-07-31 13:50:14 +0300

    Chore:STarter files & solved three codility challenges

--- question_4.py
+++ question_4.py
@@ -20,4 +20,23 @@ It should return the cost of changing the sign from the old message to the new m
 
 
 def estimate(add_cost, remove_cost, old_sign, new_sign):
-    return 0
+    m, n = len(old_sign), len(new_sign)
+    
+    matrix = [[0] * (n + 1) for _ in range(m + 1)]
+    
+    for i in range(m + 1):
+        matrix[i][0] = i * remove_cost
+    for j in range(n + 1):
+        matrix[0][j] = j * add_cost
+    
+    for i in range(1, m + 1):
+        for j in range(1, n + 1):
+            if old_sign[i - 1] == new_sign[j - 1]:
+                matrix[i][j] = matrix[i - 1][j - 1]
+            else:
+                matrix[i][j] = min(
+                    matrix[i - 1][j] + remove_cost,  
+                    matrix[i][j - 1] + add_cost      
+                )
+    
+    return matrix[m][n]
